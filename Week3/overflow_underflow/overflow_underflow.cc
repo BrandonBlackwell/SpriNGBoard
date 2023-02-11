54b43a58a237484e78087cb4b8ad3d0a2b820164
brandon_workspace <brandon@aws_workspace>
2023-02-10 17:01:25 -0800

    Week 3 exercise

--- Week3/overflow_underflow/overflow_underflow.cc
+++ Week3/overflow_underflow/overflow_underflow.cc
@@ -10,6 +10,8 @@ int main ()
     // INTEGERS NAMED s,u AND        //
     // INITIALIZE THEM TO ZERO       //
     ///////////////////////////////////
+    int32_t s{};
+    uint32_t u{};
 
 
     cout << "32-bit before decrement:" << endl;
@@ -19,7 +21,8 @@ int main ()
     ///////////////////
     // DECREMENT s,u //
     ///////////////////
-
+    --s;
+    --u;
 
     cout << "32-bit after decrement:" << endl;
     cout << "  signed:   " << s << endl;
@@ -32,8 +35,8 @@ int main ()
     // INTEGERS NAMED s,u AND        //
     // INITIALIZE THEM TO MAXIMUMS   //
     ///////////////////////////////////
-
-
+    int16_t s{};
+    uint16_t u{};
     cout << "16-bit before increment:" << endl;
     cout << "  signed:   " << s << endl;
     cout << "  unsigned: " << u << endl;
@@ -41,6 +44,8 @@ int main ()
     ////////////////////////
     // INCREMENT s,u HERE //
     ////////////////////////
+    ++s;
+    ++u;
 
 
     cout << "16-bit after increment:" << endl;
