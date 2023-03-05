3ea912bf4205ac36c8feb641bd1284f1bb2ee3dd
brandon_workspace <brandon@aws_workspace>
2023-03-05 02:32:21 -0800

    Basic Polymorphism and virtual functions

--- Week6/polymorphism/polymorphism.cc
+++ Week6/polymorphism/polymorphism.cc
@@ -7,31 +7,40 @@ class Animal
   public:
     virtual void make_sound () const = 0;
 };
-
+// Stand alone function that accepts a const animal object as param.
 void poke (Animal const & animal)
 { animal.make_sound(); }
 
 class Cow : public Animal
 {
   public:
-    virtual void make_sound () const override
+    void make_sound () const override
     { cout << "moo" << endl; }
 };
 
 class FullCow : public Cow
 {
   public:
-    virtual void make_sound () const override
+    void make_sound () const override
     { cout << "Ooof - I ate too much" << endl; }
+};
+
+class SuperFullCow : public Cow
+{
+  public:
+    virtual void make_sound () const
+    { cout << "I ate waaaaay too much" << endl; }
 };
 
 int main ()
 {
   Cow c;
   FullCow fc;
+  SuperFullCow sfc;
 
   poke(c);
   poke(fc);
+  poke(sfc);
 
   return 0;
 }
