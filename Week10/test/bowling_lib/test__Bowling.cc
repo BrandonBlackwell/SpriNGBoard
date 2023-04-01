4f1cc5e2c6a972079f49538379bc1a2dd5971484
brandon_workspace <brandon@aws_workspace>
2023-04-01 07:48:38 -0700

    Simplified first test

--- Week10/test/bowling_lib/test__Bowling.cc
+++ Week10/test/bowling_lib/test__Bowling.cc
@@ -3,15 +3,8 @@
 #include <catch2/catch_test_macros.hpp>
 
 
-TEST_CASE ("this test will fail")
+TEST_CASE ("is_complete() returns false unless player completes 10th frame.")
 {
-  REQUIRE(true);
-}
-TEST_CASE ("is_complete() returns true when every player completes their 10th frame")
-{
-  Game g;
-  g.start(p1);
-  p1.frame_count(11);
-  
-  REQUIRE(g.is_complete() == true);
+  Bowling::Game g;
+  REQUIRE(g.is_complete() == false);
 }
